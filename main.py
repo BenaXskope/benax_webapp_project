bbe646be97b75f5b2619e05d92a24d71cadfe086
BenaXskope <gyroskope225@gmail.com>
2021-04-18 20:35:00 +0300

    Commit

--- main.py
+++ main.py
@@ -8,18 +8,19 @@ urlpatterns = {
     '/about/': views.about_view,
 }
 
+front_controllers = [
+    secret_controller
+]
+
 
 def secret_controller(request):
     # пример Front Controller
     request['check'] = 'checked'
 
 
-front_controllers = [
-    secret_controller
-]
+
 
 application = Application(urlpatterns, front_controllers)
 with make_server('', 8000, application) as httpd:
     print('Server started at port: 8000')
     httpd.serve_forever()
-
